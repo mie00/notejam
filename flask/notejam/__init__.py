e1418deb6a6cca7991e91c5595e22f3f8e69a122
Mohamed Elawadi <mohamed@elawadi.net>
2019-07-09 22:38:41 +0200

    move git config from env to package

--- flask/notejam/__init__.py
+++ flask/notejam/__init__.py
@@ -1,11 +1,41 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
 
+from logging.config import dictConfig
+
 # @TODO use application factory approach
 app = Flask(__name__)
-app.config.from_object('notejam.config.Config')
+
+config = 'notejam.config.DevelopmentConfig'
+env_config = os.environ.get("APP_SETTINGS")
+if env_config:
+    config = env_config
+
+app.config.from_object(config)
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {
+        'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'default'
+        }
+    },
+    'loggers': {
+        '': {
+            'level': app.config['LOG_LEVEL'],
+            'handlers': ['wsgi']
+        }
+    }
+})
+
 db = SQLAlchemy(app)
 
 login_manager = LoginManager()
--- flask/runserver.py
+++ flask/runserver.py
@@ -4,35 +4,9 @@ from notejam import app
 from logging.config import dictConfig
 
 host = '127.0.0.1'
-config = 'notejam.config.DevelopmentConfig'
 env_config = os.environ.get("APP_SETTINGS")
 if env_config:
-    config = env_config
     host = '0.0.0.0'
-
-app.config.from_object(config)
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {
-        'wsgi': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stdout',
-            'formatter': 'default'
-        }
-    },
-    'loggers': {
-        '': {
-            'level': app.config['LOG_LEVEL'],
-            'handlers': ['wsgi']
-        }
-    }
-})
-
-
 
 if __name__ == '__main__':
     app.run(host=host)
