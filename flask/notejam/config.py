6c72ffe3217286cef647edb6efa619d770e4d334
Mohamed Elawadi <mohamed@elawadi.net>
2019-07-09 22:24:00 +0200

    prod: add mail config

--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -16,7 +16,16 @@ class ProductionConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     CSRF_SESSION_KEY = os.environ.get('CSRF_SESSION_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    LOG_LEVEL = os.environ.get('LOG_LEVEL') or 'INFO'
+    LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', False)
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', False)
+
+
 
 
 class DevelopmentConfig(Config):
